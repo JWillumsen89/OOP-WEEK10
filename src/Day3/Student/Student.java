01e53bd926a564f3d541da85f08cbb8bf709b0cb
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-03-09 09:16:55 +0100

    added yearOfBirth

--- src/Day3/Student/Student.java
+++ src/Day3/Student/Student.java
@@ -4,12 +4,20 @@ public class Student {
   private String name;
   private String phone;
   private double avgGrade;
+  private int yearOfBirth;
+
+  Student() {}
+
+  Student(int y) {
+    yearOfBirth = y;
+
+  }
 
   void setName(String n) {
     if ((!n.equals("")))
       name = n;
     else
-    name = "N/A";
+      name = "N/A";
   }
 
   String getName() {
@@ -24,19 +32,26 @@ public class Student {
     return phone;
   }
 
+  int getYearOfBirth() {
+    return yearOfBirth;
+  }
+
 }
 
 class TestStudent {
 
   public static void main(String[] args) {
-    Student s1 = new Student();
+    Student s1 = new Student(2010);
     //s1.name = "Patrick";
-  s1.setName("Patrick");
+    s1.setName("Patrick");
     System.out.println(s1.getName());
     s1.setName("");
     System.out.println(s1.getName());
 
     s1.setPhone("12 34 56 78");
     System.out.println(s1.getPhone());
+
+    System.out.println("Year: " + s1.getYearOfBirth());
+
   }
 }
